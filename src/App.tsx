104514f495c31858c896fb93ae46e185c2fa6cb3
ThanhTran16389 <tran_van_thanh_86@yahoo.com>
2025-04-01 23:32:33 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import TodoApp from './Component/TodoApp'
 function App() {
   return (
     <div>
-      <h1 className="text-blue-600 font-bold mb-4">Quản Lý Danh Bạ</h1>
+      <h1 className="text-blue-600 font-bold mb-4">
+        Quản Lý Tên Và Số Điện Thoại
+      </h1>
       <TodoApp />
     </div>
   )
